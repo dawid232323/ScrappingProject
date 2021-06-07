afd145fdc3b1b05b44c03c649069b2e7b130d3a9
Dawid Pylak <dawpylak@gmail.com>
2021-06-07 11:35:09 +0200

    work on changing mechanisms almost done

--- scrap2.py
+++ scrap2.py
@@ -15,9 +15,9 @@ class page_hanlder():
     # current_street = None
     web_driver = None
     mode  = str
-    counter = 0
-    town_counter = 0
-    municipility_counter = 0
+    counter = 1
+    town_counter = 1
+    municipility_counter = 2
     list_of_streets = None
     list_of_towns = None
     list_of_municipilities = None
@@ -34,35 +34,42 @@ class page_hanlder():
         # self.current_street = current_street
         self.web_driver = driver
         self.mode = mode
+        if self.mode == 't':
+            self.town_counter = 1
+        elif self.mode == 's':
+            self.counter = 1
 
-    def get_municipilities_list(self):
+    def get_municipilities_list(self): #function that changes municipility and gets all states and counties during initialozation. States and counties are used during the refresh
         states_list = Select(self.web_driver.find_element_by_xpath('//*[@id="selWojewodztwo"]'))
         self.current_state = states_list.first_selected_option.get_attribute('value')
         self.list_of_municipilities = Select(self.web_driver.find_element_by_xpath('//*[@id="selGmina"]'))
         self.municipilities_options = self.list_of_municipilities.options
-        self.current_municipility = self.municipilities_options[self.municipility_counter].get_attribute("value")
+        self.current_municipility = self.municipilities_options[self.municipility_counter].get_attribute("value") #current municipility is the one on the counter's position. Counter is never changed after init at tths moment
     
-    def get_street_list(self):
+    def get_street_list(self): #function that gets all the streets. It is called whenever the town is changed
         self.list_of_streets = Select(self.web_driver.find_element_by_xpath('//*[@id="selUlica"]'))
         self.options = self.list_of_streets.options
-        self.counter = 0
-        while len(self.options) == 0:
+        self.counter = 1 #counter is set to one, because 'rozwiń' is on the 0 position
+        while len(self.options) == 0: #mechanism that waits for the streets list to load
             print("czekam na listę ulic")
             time.sleep(0.3)
             self.options = self.list_of_streets.options
-        self.current_street = self.options[self.counter].get_attribute('value')
+        if len(self.options) > 1: #current street is dependent to list size. If size is greater than one it means that there are streets in the town
+            self.current_street = self.options[self.counter].get_attribute('value')
+        else:
+            self.current_street = self.options[0].get_attribute('value')
     
-    def get_towns_lists(self):
+    def get_towns_lists(self): #function that gets all the towns in a municipility. It is called after every municipility change
             self.list_of_towns = Select(self.web_driver.find_element_by_xpath('//*[@id="selMiejscowosc"]'))
             self.towns_options = self.list_of_towns.options
-            self.town_counter = 0
-            while len(self.towns_options) == 0:
+            # self.town_counter = 1 #counter is set to one, because position 0 is 'rozwiń'
+            while len(self.towns_options) == 0: #mechanism that waits for towns list to load
                 print('czekam na listę miast')
                 time.sleep(0.3)
                 self.towns_options = self.list_of_towns.options
-            self.current_town = self.towns_options[self.town_counter].get_attribute('value')
+            self.current_town = self.towns_options[self.town_counter].get_attribute('value') #current town is set to first town in the list
 
-    def empty_page(self):
+    def empty_page(self): #function that checks if page is empty 
         try:
             message = self.web_driver.find_element_by_xpath('//*[@id="divInfoKomunikat"]').text
             if message == 'Nie znaleziono podmiotów':
@@ -72,34 +79,61 @@ class page_hanlder():
         except:
             return False
 
-    def change_street(self):
-        if self.current_street == self.options[-1].get_attribute('value'):
+    def change_street(self): #function that changes street
+        if self.current_street == self.options[-1].get_attribute('value'): #if current street is the last one on the list of streets, mode is changed to towns, because at this point we don't konow
+            #if next town has streets. Then function to change town is called
+            print('ostatnia ulica')
             self.mode = 't'
             self.change_town()
             
-        else:
+        else: #if street is not the last one next one is choosen
             print('przystę[uję do zmiany ulicy')
             search_button = self.web_driver.find_element_by_xpath('//*[@id="btnSzukajPoAdresie"]')
-            wanted_street = self.options[self.counter + 1].get_attribute('value')
+            wanted_street = self.options[self.counter + 1].get_attribute('value') #this is the street that option will be changed to. It is on the next postion relative to the counter 
             self.list_of_streets.select_by_value(wanted_street)
             search_button.click()
-            self.current_street = wanted_street
-            self.counter += 1
-            self.last_number = 0
+            self.current_street = wanted_street #current street is changed to the wanted one (that's next street realtive to the current_one)
+            self.counter += 1 #position of the counter is changed
+            self.last_number = 0 #last number represents amount of pages displayed so far 
     
-    def change_municipility(self):
+    def change_municipility(self): #function that changes municipility to the next one 
         # search_button = self.web_driver.find_element_by_xpath('//*[@id="btnSzukajPoAdresie"]')
-        wanted_municipility = self.municipilities_options[self.municipility_counter + 1].get_attribute('value')    
+        wanted_municipility = self.municipilities_options[self.municipility_counter + 1].get_attribute('value') #wanted municipility is the next one relative to the current one    
         self.list_of_municipilities.select_by_value(wanted_municipility)
-        self.municipility_counter += 1
-        self.current_municipility = wanted_municipility
-        self.town_counter = 0
-        self.get_towns_lists()
+        self.municipility_counter += 1 #counter of municipilities is increased by one
+        self.current_municipility = wanted_municipility #current municipility is changed to the next one 
+        self.town_counter = 0 #town counter is 0, because in the change town function, wanted town is counter + 1, so if it was equal to 1, we would skip the first item 
+        self.counter = 1
+        time.sleep(1)
+        self.get_towns_lists() #becuase municipility is changed, we need new list of towns
+        print('pobrałem liste miast po zmianie gminy')
+        print('długość listy miast ', len(self.towns_options), 'ostatni element:', self.towns_options[-1].text)
+        self.change_town() #because town field is empty, we need to change it to the first one. Because of town counter equal to 0, wanted town in this function becomes the first one on the list 
         # search_button.click()
 
+    def search_for_popup(self):
+        try:
+            self.web_driver.switchTo().alert().accept()
+            return False
+        except Exception:
+            return True
+
     def change_town(self):
-        if self.current_town == self.towns_options[-1].get_attribute('value'):
+        if self.current_town == self.towns_options[-1].get_attribute('value') and len(self.towns_options) != 2:
+            print('ostatnie miasto', self.towns_options[-1].text)
             self.change_municipility()
+        elif self.current_town == self.towns_options[-1].get_attribute('value') and len(self.towns_options) == 2:
+            print('wchodzę w elif')
+            street_copy = self.options[1].text 
+            self.get_street_list()
+            if street_copy == self.options[1].text:
+                print('zmieniam gminę, po przejściu całego miasta')
+                self.change_municipility()
+            else:
+                self.mode = 's'
+                self.counter = 1
+            
+            self.change_street()
         else:
             search_button = self.web_driver.find_element_by_xpath('//*[@id="btnSzukajPoAdresie"]')
             wanted_town = self.towns_options[self.town_counter + 1].get_attribute('value')
@@ -109,17 +143,13 @@ class page_hanlder():
             time.sleep(0.4)
             self.get_street_list()
             print('pobrałem listę ulic po zmianie miasta')
-            self.counter = 0
-            if len(self.options) != 1:
+            self.counter = 1
+            search_button.click()
+            if not self.search_for_popup():
                 print('wszedłem w pierwszego if')
                 self.mode = 's'
-                self.counter = 0
+                self.counter = 1
                 self.change_street()
-            else:
-                print('wchpdzę w else')
-                search_button.click()
-                
-
             
     def change_selector(self):
         if self.mode == 's':
@@ -162,9 +192,48 @@ class page_hanlder():
                     self.change_selector()
 
     def emergency_refresh(self):
+        las_number_copy = self.last_number
+        current_municipility_copy = self.current_municipility
+        current_town_copy = self.current_town
+        current_street_copy = self.current_street
+        current_state_copy = self.current_state
+        current_county_copy = self.current_county
+        counters_copy = [self.counter, self.town_counter, self.municipility_counter]
         self.web_driver.refresh()
+        time.sleep(3)
         addres_button = self.web_driver.find_element_by_xpath('//*[@id="btnMenuSzukajPoAdresie"]')
         addres_button.click()
+        time.sleep(3)
+        self.list_of_states = Select(self.web_driver.find_element_by_xpath('//*[@id="selWojewodztwo"]')) 
+        self.list_of_states.select_by_value(current_state_copy)
+        time.sleep(0.5)
+        self.counties_list = Select(self.web_driver.find_element_by_xpath('//*[@id="selPowiat"]'))
+        self.counties_list.select_by_value(current_county_copy)
+        time.sleep(0.5)
+        self.list_of_municipilities = Select(self.web_driver.find_element_by_xpath('//*[@id="selGmina"]')) 
+        self.list_of_municipilities.select_by_value(current_municipility_copy)
+        time.sleep(0.5)
+        self.list_of_towns = Select(self.web_driver.find_element_by_xpath('//*[@id="selMiejscowosc"]'))
+        self.list_of_towns.select_by_value(current_town_copy)
+        time.sleep(0.5)
+        search_button = self.web_driver.find_element_by_xpath('//*[@id="btnSzukajPoAdresie"]')
+        self.get_municipilities_list()
+        self.get_towns_lists()
+        self.get_street_list()
+        self.counter = counters_copy[0]
+        self.town_counter = counters_copy[1]
+        self.municipility_counter = counters_copy[2]
+        if len(self.options) != 1:
+            self.list_of_streets.select_by_value(current_street_copy)
+        search_button.click()
+        time.sleep(0.5)
+        if int(self.last_number) > -1:
+            current_number = self.web_driver.find_element_by_xpath('//*[@id="spanPageIndex"]').text
+            result = current_number.split('/')
+            while result[0] != self.last_number:
+                self.next_page()
+                current_number = self.web_driver.find_element_by_xpath('//*[@id="spanPageIndex"]').text
+                result = current_number.split('/')
 
 
     
@@ -218,6 +287,7 @@ if __name__ == '__main__':
     i = 0
     pageHandler.get_municipilities_list()
     pageHandler.get_towns_lists()
+    pageHandler.get_street_list()
     while True:
         pageHandler.check_status()
         time.sleep(1)
