af483cac3a0bff6c05ada2644a1a9f6732cc5129
Dawid Pylak <dawpylak@gmail.com>
2021-09-23 21:05:18 +0200

    finished functions and emplyment, fixed switch, started working on research

--- scientists.py
+++ scientists.py
@@ -3,6 +3,16 @@ import pandas as pd
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class data_collector():
     def __init__(self, driver):
@@ -10,6 +20,9 @@ class data_collector():
         self.main_data = {'ID': None, 'Imie': None, 'Nazwisko': None, 'Specjalności' : None, 'Klasyfikacja': None, 'Powiązane profile': None,
         'Studia': None, 'Miejsce': None, 'Rok ukończenia': None}
         self.employment = []
+        self.functions = []
+        self.research = []
+        self.control_number = 1
 
     def __check_presence(self, xpath: str, desired_text = 'default') -> bool:
         try:
@@ -30,26 +43,24 @@ class data_collector():
             rd_item = item.text
             if rd_item == 'STUDIA':
                 self.__education_collector()
+                self.control_number += 1
             elif rd_item == 'STOPNIE I TYTUŁY':
                 self.__titles_collector()
-            # elif rd_item == 'ZATRUDNIENIE':
-            #     self.__employment_collector()
-        # if self.__check_presence('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[1]/div/div/div/h2', 'STUDIA'): #checking if education box is present 
-        #     self.__education_collector() #collecting eduaction data 
-        # if self.__check_presence('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[2]/div/div/div/h2', 'STOPNIE I TYTUŁY'): #checking if titles and degrees are present 
-        #     print('entering collector')
-        #     self.__titles_collector()
-        # if self.__check_presence('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[1]/div/div/div/h2', 'ZATRUDNIENIE'): #checking if employment box is present
-        #     print('employment checked')
-        #     self.__employment_collector()
-        print(self.main_data)
-        # return self.main_data
-         
+                self.control_number += 1
+            elif rd_item == 'ZATRUDNIENIE':
+                self.__employment_functions_collector('emp')
+                self.control_number += 1 
+            elif rd_item == 'FUNKCJE I CZŁONKOSTWA':
+                self.__employment_functions_collector('func')
+                self.control_number += 1
+            elif rd_item == 'PRACE BADAWCZE':
+                self.__research_collector()
+                self.control_number += 1
 
     def __education_collector(self) -> None:
-        self.main_data['Studia'] = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[1]/div/div/div/section/div/div/h4').text
-        self.main_data['Miejsce'] = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[1]/div/div/div/section/div/div/div/div[1]').text
-        self.main_data['Rok ukończenia'] = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[1]/div/div/div/section/div/div/div/div[2]/div[2]/span').text
+        self.main_data['Studia'] = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[%d]/div/div/div/section/div/div/h4' % self.control_number).text
+        self.main_data['Miejsce'] = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[%d]/div/div/div/section/div/div/div/div[1]' % self.control_number).text
+        self.main_data['Rok ukończenia'] = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[%d]/div/div/div/section/div/div/div/div[2]/div[2]/span' % self.control_number).text
 
     def  __summary_data_collector(self) -> None:
         headers = ('Specjalności', 'Klasyfikacja', 'Powiązane profile')
@@ -72,7 +83,7 @@ class data_collector():
 
     def __titles_collector(self) -> None:
        headers = ('Profesor', 'Habilitacja', 'Doktorat')
-       block = self.web_driver.find_elements_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[2]/div/div/div/section/div')
+       block = self.web_driver.find_elements_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[%d]/div/div/div/section/div' % self.control_number)
        split_items = block[0].text.split('\n')
        for item in split_items:
             if item in headers:
@@ -87,8 +98,8 @@ class data_collector():
                 else:
                     self.main_data[base_name + '_default'] = ready_key[0]
 
-    def __employment_collector(self) -> None: 
-        employment_dict = {'ID': None, 'Type': None, 'Position': None, 'Place': None}
+    def __employment_functions_collector(self, prefix:str) -> None: 
+        output_dictionary = {prefix + '_ID': None, prefix + '_Type': None, prefix + '_Position': None, prefix +'_Place': None}
         temp = self.web_driver.find_elements_by_class_name('nnp-tab-item-title')
         positions = set()
         for item in temp:
@@ -96,36 +107,62 @@ class data_collector():
         del(temp)
         current_type = str
         types = ('AKTUALNE', 'HISTORYCZNE')
-        block = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[3]/div/div/div/section').text 
+        block = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[%d]/div/div/div/section' % self.control_number).text 
         #reads whole block with employment rows, then it is converted to simple string and split by new line 
         split_block = block.split('\n')
-        print(split_block)
         i = 0
         while i != len(split_block) - 1: 
             if ''.join(re.findall(r'[A-Z]{11}|[A-Z]{8}', split_block[i])) in types:
                 current_type = ''.join(re.findall(r'[A-Z]{11}|[A-Z]{8}', split_block[i]))
                 i += 1
             elif split_block[i] in positions:
-                employment_dict['ID'] = self.main_data['ID']
-                employment_dict['Type'] = current_type
-                employment_dict['Position'] = split_block[i]
-                employment_dict['Place'] = split_block[i + 1]
+                output_dictionary[prefix + '_ID'] = self.main_data['ID']
+                output_dictionary[prefix + '_Type'] = current_type
+                output_dictionary[prefix + '_Position'] = split_block[i]
+                output_dictionary[prefix +'_Place'] = split_block[i + 1]
                 i += 1 
             else:
-                employment_dict['ID'] = self.main_data['ID']
-                employment_dict['Type'] = current_type
-                employment_dict['Position'] = None
-                employment_dict['Place'] = split_block[i]
+                output_dictionary[prefix + '_ID'] = self.main_data['ID']
+                output_dictionary[prefix + '_Type'] = current_type
+                output_dictionary[prefix + '_Position'] = None
+                output_dictionary[prefix +'_Place'] = split_block[i]
                 i += 1
-            if employment_dict['Place'] != None and employment_dict['ID'] != None:
-                self.employment.append(employment_dict)
-                employment_dict.clear() 
-        print(self.employment)
+            if output_dictionary[prefix +'_Place'] != None and output_dictionary[prefix + '_ID'] != None:
+                if prefix == 'emp':
+                    self.employment.append(output_dictionary)
+                elif prefix == 'func':
+                    self.functions.append(output_dictionary)    
+                output_dictionary = {prefix + '_ID': None, prefix + '_Type': None, prefix + '_Position': None, prefix +'_Place': None}
+        print(colors().OKGREEN, self.employment, colors().ENDC)
+        print(colors().OKGREEN, self.functions, colors().ENDC)
 
-    def __test_function(self):
-        print('testtinfg')
-        elem = self.web_driver.find_elements_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[3]/div/div/div/h2')
-        print(elem[0].text)
+    def __research_collector(self):
+        jobs = ('Recenzent', 'Promotor', 'Kierownik', 'Wykonawca')
+        research_dictionary = {'res_ID':None, 'res_Job': None, 'res_Title': None, 'res_reesearchID': None, 'res_Osoby_powiązane_z_pracą': None, 'res_Instytucje_powiązane_z_pracą': None}
+        block = block = self.web_driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[%d]/div/div/div/section' % self.control_number).text 
+        split_block = block.split('\n')
+        current_job = str
+        i = 0
+        while i != len(split_block) - 1:
+            if re.sub('[\ ]*Mniej', '', split_block[i]) in jobs:
+                current_job = re.sub('[\ ]*Mniej', '', split_block[i])
+                i += 1
+            else:
+                research_dictionary['res_ID'] = self.main_data['ID']
+                research_dictionary['res_Job'] = current_job
+                research_dictionary['res_Title'] = re.findall(r'[^1-9]\ .*', split_block[i])
+                research_dictionary['res_researchID'] = split_block[i + 1]
+                research_dictionary['res_Osoby_powiązane_z_pracą'] = split_block[i + 3]
+                research_dictionary['res_Instytucje_powiązane_z_pracą'] = split_block[i + 5]
+                self.research.append(research_dictionary)
+                research_dictionary = {'res_ID':None, 'res_Job': None, 'res_Title': None, 'res_reesearchID': None, 'res_Osoby_powiązane_z_pracą': None, 'res_Instytucje_powiązane_z_pracą': None}
+                i += 4
+
+        for item in self.research:
+            print(item)
+
+    def data_getter(self):
+        return self.main_data, self.employment, self.functions, self.research
         
         
 
@@ -134,9 +171,9 @@ def main():
     dictionaries = []
     driver = webdriver.Chrome()
     # driver.get('https://nauka-polska.pl/#/profile/scientist?id=202533&_k=5py0p0')
-    # driver.get('https://nauka-polska.pl/#/profile/scientist?id=27951&_k=iid8du')
+    driver.get('https://nauka-polska.pl/#/profile/scientist?id=27951&_k=iid8du')
     # driver.get('https://nauka-polska.pl/#/profile/scientist?id=225261&_k=2m5mzl')
-    driver.get('https://nauka-polska.pl/#/profile/scientist?id=25983&_k=488av6')
+    # driver.get('https://nauka-polska.pl/#/profile/scientist?id=25983&_k=488av6')
     # driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[2]/div/div/div/section/div/div[3]/a').click()
     start = input('start ')
     data_collector(driver).switcher()
@@ -146,6 +183,6 @@ if __name__ == '__main__':
     main()
 
 
-//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[2]/div/div/div
-//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[1]/div/div/div
-//*[@id="content"]/div/div[2]/div[1]/main/section/section/section[3]/div/div/div
+# //*[@id="content"]/div/div[2]/div[1]/main/section/section/section[2]/div/div/div
+# //*[@id="content"]/div/div[2]/div[1]/main/section/section/section[1]/div/div/div
+# //*[@id="content"]/div/div[2]/div[1]/main/section/section/section[3]/div/div/div
